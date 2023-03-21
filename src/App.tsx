c2036d1443e131b48aaaefc979a12fec96a9176d
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-03-21 18:40:10 +0300

    done 15 tasks

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,45 @@ import './App.css';
 import Shoplist from './components/Shoplist';
 import {v1} from 'uuid';
 
-export type GoodsType = {
+export type Shoplist = {
     id: string
     title: string
-    inBacket: boolean
+    filter: FilterType
 }
+
+export type GoodType = {
+    id: string
+    title: string
+    inBacket: boolean}
+
+export type GoodsType = {
+    [key:string]:GoodType[];
+}
+
 export type FilterType = 'all' | 'need to buy' | 'in basket';
 
 function App() {
 
-    const [goods, setGoods] = useState<GoodsType[]>([
-        {id: v1(), title: 'Milk', inBacket: true},
-        {id: v1(), title: 'Bear', inBacket: false},
-        {id: v1(), title: 'Book', inBacket: false},
+    const shoplistID1 = v1();
+    const shoplistID2 = v1();
+
+    const [shoplists, setShoplists] = useState<Shoplist[]>([
+        {id: shoplistID1, title: 'shop today', filter: 'all'},
+        {id: shoplistID2, title: 'shop later', filter: 'all'},
     ])
+
+    const [goods, setGoods] = useState<GoodsType>({
+        [shoplistID1]: [
+            {id: v1(), title: 'Milk', inBacket: true},
+            {id: v1(), title: 'Bear', inBacket: false},
+            {id: v1(), title: 'Book', inBacket: false},
+        ],
+        [shoplistID1]: [
+            {id: v1(), title: 'Milk', inBacket: true},
+            {id: v1(), title: 'Bear', inBacket: false},
+            {id: v1(), title: 'Book', inBacket: false},
+        ]
+    })
 
     const [filter, setFilter] = useState<FilterType>('all')
 
